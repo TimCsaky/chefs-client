5caceae772a9d95a00a3d5b84f6b29e05722b85f
Csaky <Tim.Csaky@gov.bc.ca>
2020-07-21 10:14:05 -0700

    two forms, using local dev formio api

---
+++ app/frontend/src/views/SecureForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <FormTwo />
+  </v-container>
+</template>
+
+<script>
+import FormTwo from '@/components/forms/FormTwo.vue';
+export default {
+  name: 'Form',
+  components: {
+    FormTwo
+  }
+};
+</script>
